49814f6e24620451ced3727af0173d5e35271b6e
shilpa <shilpa.t1@hotmail.com>
2018-07-31 16:24:14 -0400

    SeleniumWebDriver_Maven

---
+++ Documents/Study_Stuff/Selenium_Project/Eclipse_Project_WorkPath/module1/src/test/java/com/seleniumproject/module1/NewTestNG.java
@@ -0,0 +1,26 @@
+package com.seleniumproject.module1;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class NewTestNG {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\shilpa\\Documents\\Study_Stuff\\Selenium_Project\\Selenium_Browser\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	

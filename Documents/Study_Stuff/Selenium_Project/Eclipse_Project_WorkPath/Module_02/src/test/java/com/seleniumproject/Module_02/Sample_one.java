c704c4fd1cc192259c0c19997a0986b8cf240132
shilpa <shilpa.t1@hotmail.com>
2018-08-02 15:51:15 -0400

    Adding new project to new repository

---
+++ Documents/Study_Stuff/Selenium_Project/Eclipse_Project_WorkPath/Module_02/src/test/java/com/seleniumproject/Module_02/Sample_one.java
@@ -0,0 +1,27 @@
+package com.seleniumproject.Module_02;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;		
+public class Sample_one {		
+	    private WebDriver driver;		
+		@Test				
+		public void testEasy() {	
+			driver.get("https://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+			System.out.println("hello");
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+			System.setProperty("webdriver.gecko.driver","C:\\Users\\shilpa\\Documents\\Study_Stuff\\Selenium_Project\\Selenium_Browser\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}		
+}	
